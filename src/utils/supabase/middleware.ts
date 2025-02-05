3430ebedace988ff5f18a90ef57581273cecbb08
Moaz Mirza <moazmirza13@gmail.com>
2025-02-05 10:35:07 +0500

    supabase setup

---
+++ src/utils/supabase/middleware.ts
@@ -0,0 +1,59 @@
+import { createServerClient } from '@supabase/ssr';
+import { type NextRequest, NextResponse } from 'next/server';
+
+export const updateSession = async (request: NextRequest) => {
+  try {
+    let response = NextResponse.next({
+      request: {
+        headers: request.headers
+      }
+    });
+
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return request.cookies.getAll();
+          },
+          setAll(cookiesToSet) {
+            cookiesToSet.forEach(({ name, value }) =>
+              request.cookies.set(name, value)
+            );
+            response = NextResponse.next({
+              request
+            });
+            cookiesToSet.forEach(({ name, value, options }) =>
+              response.cookies.set(name, value, options)
+            );
+          }
+        }
+      }
+    );
+
+    // This will refresh session if expired - required for Server Components
+    // https://supabase.com/docs/guides/auth/server-side/nextjs
+    const user = await supabase.auth.getUser();
+    console.log('🚀 ~ updateSession ~ user:', user);
+
+    // protected routes
+    if (request.nextUrl.pathname.startsWith('/notes') && user.error) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+
+    if (request.nextUrl.pathname === '/' && !user.error) {
+      return NextResponse.redirect(new URL('/notes', request.url));
+    }
+
+    return response;
+  } catch (e) {
+    // If you are here, a Supabase client could not be created!
+    // This is likely because you have not set up environment variables.
+    return NextResponse.next({
+      request: {
+        headers: request.headers
+      }
+    });
+  }
+};
