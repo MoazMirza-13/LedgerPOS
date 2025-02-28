16ba706a3649021886e6a827c02aa37fa62c1c3e
Moaz Mirza <moazmirza13@gmail.com>
2025-02-28 23:56:54 +0500

    table in view page POC

--- src/features/dynamic/dynamic-form.tsx
+++ src/features/dynamic/dynamic-form.tsx
@@ -14,17 +14,21 @@ import {
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { categoryBrandSubmit } from '@/lib/actions';
-import { toastMsg } from '@/lib/utils';
+import { getClientImageUrl, getImageUrl, toastMsg } from '@/lib/utils';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { LoaderCircle } from 'lucide-react';
 import { usePathname, useRouter } from 'next/navigation';
-import { useTransition } from 'react';
+import { Suspense, use, useActionState, useTransition } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
-import { Brand, Category } from 'types';
+import { Brand, Category, itemTable, Product } from 'types';
 import * as z from 'zod';
 import AddProductButton from '../../components/ui/add-product';
 import Link from 'next/link';
+import TableClientSide from './table-components/tableClient';
+import { createClient } from '@/utils/supabase/client';
+import { useQuery } from '@tanstack/react-query';
+import PageContainer from '@/components/layout/page-container';
 
 export default function DynamicForm({
   initialData,
@@ -75,71 +79,126 @@ export default function DynamicForm({
   const newPath = pathname.includes('new');
   const newLink = `/dashboard/products/new?${title.toLowerCase()}=${initialData?.title}`;
 
+  const getProducts = async () => {
+    const supabase = createClient();
+    let data;
+    const { data: productsData, error } = await supabase.from('products')
+      .select(`
+            *,
+            categories (title),
+            brands (title)
+          `);
+    if (error) return;
+
+    const productsWithImg = productsData
+      ? await Promise.all(
+          productsData.map(async (product) => ({
+            ...product,
+            img_url: await getClientImageUrl(product.img_url)
+          }))
+        )
+      : null;
+
+    data = productsWithImg;
+
+    return data?.reverse();
+  };
+
+  const {
+    data: productsData,
+    isLoading,
+    error,
+    refetch
+  } = useQuery({
+    queryKey: ['products', type],
+    queryFn: getProducts,
+    enabled: false // This prevents auto-fetching on component mount
+  });
+
   return (
-    <Card className='mx-auto w-full'>
-      <CardHeader className='flex flex-row items-center justify-between'>
-        <CardTitle className='text-left text-2xl font-bold'>
-          {pageTitle}
-        </CardTitle>
-        {!newPath && (
-          <Link href={newLink}>
-            <AddProductButton />
-          </Link>
-        )}
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(handleFormSubmit)}
-            className='space-y-8'
-          >
-            <div className='grid grid-cols-1 gap-6 md:grid-cols-2'>
+    <>
+      <Card className='mx-auto w-full'>
+        <CardHeader className='flex flex-row items-center justify-between'>
+          <CardTitle className='text-left text-2xl font-bold'>
+            {pageTitle}
+          </CardTitle>
+          {!newPath && (
+            <Link href={newLink}>
+              <AddProductButton />
+            </Link>
+          )}
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleFormSubmit)}
+              className='space-y-8'
+            >
+              <div className='grid grid-cols-1 gap-6 md:grid-cols-2'>
+                <FormField
+                  control={form.control}
+                  name='title'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{`${title} Name`}</FormLabel>
+                      <FormControl>
+                        <Input placeholder={`Enter ${title}`} {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
               <FormField
                 control={form.control}
-                name='title'
+                name='description'
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>{`${title} Name`}</FormLabel>
+                    <FormLabel>Description</FormLabel>
                     <FormControl>
-                      <Input placeholder={`Enter ${title}`} {...field} />
+                      <Textarea
+                        placeholder={`Enter ${title.toLowerCase()} description`}
+                        className='resize-none'
+                        {...field}
+                      />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-            </div>
-            <FormField
-              control={form.control}
-              name='description'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder={`Enter ${title.toLowerCase()} description`}
-                      className='resize-none'
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+              <Button type='submit' disabled={isPending}>
+                {isPending ? (
+                  <div className='flex gap-2'>
+                    {initialData ? `Editing ` : `Adding`}
+                    <LoaderCircle className='h-5 w-5 animate-spin' />
+                  </div>
+                ) : initialData ? (
+                  `Edit ${title}`
+                ) : (
+                  `Add ${title}`
+                )}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+      <div>
+        <Button onClick={() => refetch()}>View Products</Button>
+      </div>
+      {productsData && (
+        <PageContainer scrollable={false}>
+          <div
+            className='flex flex-1 flex-col space-y-4'
+            style={{ minHeight: '400px' }}
+          >
+            <TableClientSide
+              type={'products'}
+              data={productsData as Product[]}
+              total={productsData?.length as number}
             />
-            <Button type='submit' disabled={isPending}>
-              {isPending ? (
-                <div className='flex gap-2'>
-                  {initialData ? `Editing ` : `Adding`}
-                  <LoaderCircle className='h-5 w-5 animate-spin' />
-                </div>
-              ) : initialData ? (
-                `Edit ${title}`
-              ) : (
-                `Add ${title}`
-              )}
-            </Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+          </div>
+        </PageContainer>
+      )}
+    </>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { getSupabaseClient } from './actions';
 import { validate as uuidValidate } from 'uuid';
+import { createClient } from '@/utils/supabase/client';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -40,6 +41,15 @@ export const toastMsg = {
 
 export const getImageUrl = async (img: string) => {
   const supabase = await getSupabaseClient();
+  const imgUrl = img
+    ? supabase.storage.from('product_imgs').getPublicUrl(img).data.publicUrl
+    : null;
+  return imgUrl;
+};
+
+// for client side components
+export const getClientImageUrl = async (img: string) => {
+  const supabase = createClient();
   const imgUrl = img
     ? supabase.storage.from('product_imgs').getPublicUrl(img).data.publicUrl
     : null;
