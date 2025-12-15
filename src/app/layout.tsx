76c4e9e20471f56cb96adbde47ca771e40906828
Moaz Mirza <moazmirza13@gmail.com>
2025-12-16 01:14:37 +0500

    test: vercel domain config for kt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  console.log('kt');
   return (
     <html lang='en' className={`${lato.className}`} suppressHydrationWarning>
       <body className={'overflow-hidden'} suppressHydrationWarning>
