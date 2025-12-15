fbeb8ff21dfa8973a4e4acb15b03930b3ab10065
Moaz Mirza <moazmirza13@gmail.com>
2025-12-16 02:33:15 +0500

    chore: removed "m" log from client

--- src/features/auth/components/user-auth-form.tsx
+++ src/features/auth/components/user-auth-form.tsx
@@ -35,8 +35,6 @@ export default function UserAuthForm() {
     }
   });
 
-  console.log('m');
-
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
   const { isDirty } = form.formState;
