2d98f51dae5f9c69b8bca993e648a8719cd9f9e5
Moaz Mirza <moazmirza13@gmail.com>
2025-12-16 01:11:41 +0500

    test: vercel domain config for ns

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  console.log('ns');
   return (
     <html lang='en' className={`${lato.className}`} suppressHydrationWarning>
       <body className={'overflow-hidden'} suppressHydrationWarning>
